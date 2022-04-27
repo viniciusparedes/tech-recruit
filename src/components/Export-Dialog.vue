72b56b6ce5e00d15e714672f13926828882080b9
Marcus Paredes <marcus.paredes-ext@olxbr.com>
2022-04-27 15:49:45 -0300

    Finish forms components

--- src/components/Export-Dialog.vue
+++ src/components/Export-Dialog.vue
@@ -14,7 +14,233 @@
         <span class="text-h5">Concluir Avaliação</span>
       </v-card-title>
       <v-card-text>
-        <v-text-field label="Nome do Candidato" filled></v-text-field>
+
+        <v-tabs fixed-tabs v-model="tab">
+          <v-tab :href="'#dados_do_candidato'">Candidato</v-tab>
+          <v-tab :href="'#habilidades_especificas'">Habilidades Específicas</v-tab>
+          <v-tab :href="'#observacoes'">Observações</v-tab>
+          <v-tab :href="'#avaliacao'">Avaliação</v-tab>
+          <v-tab :href="'#recomendacoes_para_o_candidato'">Recomendações</v-tab>
+          <v-tab :href="'#dados_do_avaliador'">Avaliador</v-tab>
+        </v-tabs>
+
+        <v-tabs-items v-model="tab">
+          <v-tab-item class="tab-content" id="dados_do_candidato">
+            <h3>Dados do Candidato:</h3>
+            <v-row>
+              <v-col cols="5">
+                <v-text-field v-model="dados_do_candidato.nome_do_candidato" label="Nome do Candidato" outlined></v-text-field>
+              </v-col>
+              <v-col cols="2">
+                <v-select
+                  v-model="dados_do_candidato.ultima_posicao"
+                  :items="this.$niveis_profissionais"
+                  label="Última Posição"
+                  outlined
+                ></v-select>
+              </v-col>
+              <v-col cols="2">
+                <v-select
+                  v-model="dados_do_candidato.posicao_almejada"
+                  :items="this.$niveis_profissionais"
+                  label="Posição Almejada"
+                  outlined
+                ></v-select>
+              </v-col>
+              <v-col cols="3">
+                <v-text-field v-model="dados_do_candidato.tecnologia" label="Tecnologia" outlined></v-text-field>
+              </v-col>
+            </v-row>
+          </v-tab-item>
+
+
+          <v-tab-item class="tab-content" id="habilidades_especificas">
+            <h3>Habilidades Específicas:</h3>
+            <v-simple-table v-show="habilidades_especificas.length > 0" dense class="table-bordered">
+              <template v-slot:default>
+                <thead>
+                  <tr>
+                    <th class="text-left">Descrição</th>
+                    <th class="text-left">Nota</th>
+                    <th class="text-left">Justificativa</th>
+                    <th style="width:5%"></th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr v-for="(habilidade, i) in habilidades_especificas" :key="habilidade.descricao">
+                    <td>{{ habilidade.descricao }}</td>
+                    <td>{{ habilidade.nota }}</td>
+                    <td>{{ habilidade.justificativa }}</td>
+                    <td><a @click="del_habilidade(i)"><v-icon>mdi-delete</v-icon></a></td>
+                  </tr>
+                </tbody>
+              </template>
+            </v-simple-table>
+            <v-row v-show="!habilidades_especificas_form.show">
+              <v-col class="text-center">
+                <v-btn @click="habilidades_especificas_form.show = true">
+                  <v-icon dark>mdi-plus</v-icon>
+                  Adicionar
+                </v-btn>
+              </v-col>
+            </v-row>
+            <v-row v-show="habilidades_especificas_form.show">
+              <v-col cols="4">
+                <v-text-field v-model="habilidades_especificas_form.descricao" label="Descrição" outlined></v-text-field>
+              </v-col>
+              <v-col cols="2">
+                <v-select
+                  v-model="habilidades_especificas_form.nota"
+                  :items="this.$niveis"
+                  label="Nota"
+                  outlined
+                ></v-select>
+              </v-col>
+              <v-col cols="6">
+                <v-text-field v-model="habilidades_especificas_form.justificativa" label="Justificativa" outlined></v-text-field>
+              </v-col>
+              <v-col class="text-right">
+                <v-btn @click="habilidades_especificas_form.show = false">Cancelar</v-btn>
+                <v-btn color="success" @click="add_habilidade()" class="ml-2">Salvar</v-btn>
+              </v-col>
+            </v-row>
+          </v-tab-item>
+
+
+          <v-tab-item class="tab-content" id="observacoes">
+            <h3>Observações:</h3>
+            <v-row>
+              <v-col>
+                <v-textarea outlined label="Observações" v-model="observacoes"></v-textarea>
+              </v-col>
+            </v-row>
+          </v-tab-item>
+
+
+          <v-tab-item class="tab-content" id="avaliacao">
+            <h3>Avaliação:</h3>
+            <v-row>
+              <v-col cols="4">
+                <v-dialog
+                  ref="dialog"
+                  v-model="modal_date"
+                  :return-value.sync="date"
+                  persistent
+                  width="290px"
+                >
+                  <template v-slot:activator="{ on, attrs }">
+                    <v-text-field
+                      v-model="avaliacao.data"
+                      label="Data"
+                      prepend-icon="mdi-calendar"
+                      v-bind="attrs"
+                      v-on="on" outlined
+                    ></v-text-field>
+                  </template>
+                  <v-date-picker v-model="avaliacao.data" scrollable>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="modal_date = false"
+                    >
+                      Fechar
+                    </v-btn>
+                    <v-btn
+                      text
+                      color="primary"
+                      @click="$refs.dialog.save(avaliacao.data)"
+                    >
+                      OK
+                    </v-btn>
+                  </v-date-picker>
+                </v-dialog>
+              </v-col>
+              <v-col cols="4">
+                <v-select
+                  v-model="avaliacao.posicao"
+                  :items="this.$niveis_profissionais"
+                  label="Posição"
+                  outlined
+                ></v-select>
+              </v-col>
+              <v-col cols="4">
+                <v-select
+                  v-model="avaliacao.nivel"
+                  :items="['I', 'II', 'III']"
+                  label="Nível"
+                  outlined
+                ></v-select>
+              </v-col>
+              <v-col>
+                <v-textarea outlined label="Justificativa" v-model="avaliacao.justificativa"></v-textarea>
+              </v-col>
+            </v-row>
+          </v-tab-item>
+
+
+          <v-tab-item class="tab-content" id="recomendacoes_para_o_candidato">
+            <h3>Recomendações para o Candidato:</h3>
+            <v-row>
+              <v-simple-table v-show="recomendacoes.length > 0" dense class="table-bordered mt-3">
+                <template v-slot:default>
+                  <thead>
+                    <tr>
+                      <th class="text-left">Tópico</th>
+                      <th class="text-left">Recomendação</th>
+                      <th style="width:5%"></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="(recomendacao, i) in recomendacoes" :key="recomendacao.recomendacao">
+                      <td>{{ recomendacao.topico }}</td>
+                      <td>{{ recomendacao.recomendacao }}</td>
+                      <td><a @click="del_recomendacao(i)"><v-icon>mdi-delete</v-icon></a></td>
+                    </tr>
+                  </tbody>
+                </template>
+              </v-simple-table>
+            </v-row>
+            <v-row v-show="!recomendacoes_form.show">
+              <v-col class="text-center">
+                <v-btn @click="recomendacoes_form.show = true">
+                  <v-icon dark>mdi-plus</v-icon>
+                  Adicionar
+                </v-btn>
+              </v-col>
+            </v-row>
+            <v-row v-show="recomendacoes_form.show">
+              <v-col cols="5">
+                <v-text-field v-model="recomendacoes_form.topico" label="Tópico" outlined></v-text-field>
+              </v-col>
+              <v-col cols="7">
+                <v-text-field v-model="recomendacoes_form.recomendacao" label="Recomendação" outlined></v-text-field>
+              </v-col>
+              <v-col class="text-right">
+                <v-btn @click="recomendacoes_form.show = false">Cancelar</v-btn>
+                <v-btn color="success" @click="add_recomendacao()" class="ml-2">Salvar</v-btn>
+              </v-col>
+            </v-row>
+          </v-tab-item>
+
+
+          <v-tab-item class="tab-content" id="dados_do_avaliador">
+            <h3>Dados do Avaliador:</h3>
+            <v-row>
+              <v-col>
+                <v-text-field v-model="avaliador.nome" label="Nome" outlined></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field v-model="avaliador.email" label="E-Mail" outlined></v-text-field>
+              </v-col>
+              <v-col>
+                <v-text-field v-model="avaliador.especialidade" label="Especialidade" outlined></v-text-field>
+              </v-col>
+            </v-row>
+          </v-tab-item>
+
+        </v-tabs-items>
+        
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
@@ -30,8 +256,71 @@ export default {
   name: 'ExportDialog',
 
   data: () => ({
-    dialog: false
-  })
+    tab: null,
+    modal_date: false,
+    dialog: false,
+    dados_do_candidato: {
+      nome_do_candidato: '',
+      ultima_posicao: null,
+      posicao_almejada: null,
+      tecnologia: ''
+    },
+    habilidades_especificas_form: {
+      show: false,
+      descricao: '',
+      nota: null,
+      justificativa: ''
+    },
+    habilidades_especificas: [],
+    observacoes: '',
+    avaliacao: {
+      data: null,
+      posicao: null,
+      nivel: null,
+      justificativa: ''
+    },
+    recomendacoes_form: {
+      show: false,
+      topico: '',
+      recomendacao: ''
+    },
+    recomendacoes: [],
+    avaliador: {
+      nome: '',
+      email: '',
+      especialidade: ''
+    }
+  }),
+
+  methods: {
+    add_habilidade: function(){
+      let hab = this.habilidades_especificas_form
+      delete hab.show
+      this.habilidades_especificas.push(hab)
+      this.habilidades_especificas_form = {
+        show: false,
+        descricao: '',
+        nota: null,
+        justificativa: ''
+      }
+    },
+    del_habilidade: function(index){
+      this.habilidades_especificas.splice(index, 1)
+    },
+    add_recomendacao: function(){
+      let rec = this.recomendacoes_form
+      delete rec.show
+      this.recomendacoes.push(rec)
+      this.recomendacoes_form = {
+        show: false,
+        topico: '',
+        recomendacao: ''
+      }
+    },
+    del_recomendacao: function(index){
+      this.recomendacoes.splice(index, 1)
+    }
+  }
 }
 </script>
 
@@ -43,5 +332,15 @@ export default {
   position: absolute;
   right: 5px;
   top: 8px;
+}
+.table-bordered{
+  border: 1px solid silver; 
+  border-radius: 3px;
+  margin-bottom: 1rem;
+  width: 100%;
+}
+.tab-content{
+  padding: 1rem;
+  height: 60vh;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,21 @@ import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 
+Vue.prototype.$niveis_profissionais = [
+    'Iniciante',
+    'Junior',
+    'Pleno',
+    'Senior',
+    'Especialista'
+]
+Vue.prototype.$niveis = [
+    'Não possui conhecimento',
+    'Básico',
+    'Intermediário',
+    'Avançado',
+    'Especialista'
+]
+
 new Vue({
     vuetify,
     render: h => h(App)
